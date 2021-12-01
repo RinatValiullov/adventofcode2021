ae52a1bdaf0cbb10a369fac1f99acff85aa668b3
Rinat Valiullov <4rontender@gmail.com>
2021-12-02 00:21:30 +0500

    update solution (convert strings to numbers)

--- day1/solution.js
+++ day1/solution.js
@@ -1,10 +1,10 @@
 const arr = fetch("https://adventofcode.com/2021/day/1/input")
   .then((data) => data.text())
   .then((nums) => nums.split("\n"))
-  .then((nums) => nums.filter((digit) => digit.codePointAt() !== undefined))
+  .then((strings) => strings.map((str) => parseInt(str, 10)))
   .then((digits) =>
     digits.filter((num, idx, arr) => {
       return arr[idx] < arr[++idx];
     })
   )
-  .then((result) => console.log(result.length + 1));
+  .then((result) => console.log(result.length));
